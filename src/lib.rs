4787b40f878ae4c5fbd6b82cc2e118892ae4a51f
snylonue <snylonue@gmail.com>
2023-11-22 19:53:39 +0800

    rename operation to monoid and make some adjustments

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,44 @@
 pub mod operation;
 
-use std::{
-    marker::PhantomData,
-    ops::{Bound, Range, RangeBounds, RangeInclusive},
-};
+use std::ops::{Bound, Range, RangeBounds, RangeInclusive};
 
-use operation::Operation;
+use operation::Monoid;
 
-pub struct SegTree<T, O> {
+pub struct SegTree<T, M> {
     store: Vec<T>,
     len: usize,
-    _op: PhantomData<O>,
+    monoid: M,
 }
 
-impl<T: Clone, O: Operation<T>> SegTree<T, O> {
-    pub fn new(v: Vec<T>) -> Self {
+impl<T: Clone, M: Monoid<T>> SegTree<T, M> {
+    pub fn new(v: Vec<T>, monoid: M) -> Self {
         let len = if v.is_empty() {
             return Self {
                 store: Vec::new(),
                 len: 0,
-                _op: PhantomData,
+                monoid,
             };
         } else {
             2_f64.powf((v.len() as f64).log2() + 1.0).ceil() as usize - 1
         };
-        let mut store = vec![O::unit(); len];
-        Self::build(&v, &mut store, 1, v.len(), 1);
+        let mut store = vec![monoid.unit(); len];
+        Self::build(&v, &mut store, &monoid, 1, v.len(), 1);
 
         Self {
             store,
             len: v.len(),
-            _op: PhantomData,
+            monoid,
         }
     }
 
-    fn build(v: &[T], store: &mut [T], s: usize, t: usize, p: usize) {
+    fn build(v: &[T], store: &mut [T], monoid: &M, s: usize, t: usize, p: usize) {
         if s == t {
             store[p - 1] = v[s - 1].clone();
         } else {
             let m = s + (t - s) / 2;
-            Self::build(v, store, s, m, p * 2);
-            Self::build(v, store, m + 1, t, p * 2 + 1);
-            store[p - 1] = O::combine(&store[p * 2 - 1], &store[p * 2]);
+            Self::build(v, store, monoid, s, m, p * 2);
+            Self::build(v, store, monoid, m + 1, t, p * 2 + 1);
+            store[p - 1] = monoid.combine(&store[p * 2 - 1], &store[p * 2]);
         }
     }
 
@@ -73,15 +70,15 @@ impl<T: Clone, O: Operation<T>> SegTree<T, O> {
             self.store[p - 1].clone()
         } else {
             let m = cur.start() + (cur.end() - cur.start()) / 2;
-            let mut sum = O::unit();
+            let mut sum = self.monoid.unit();
             if range.start <= m {
-                sum = O::combine(
+                sum = self.monoid.combine(
                     &sum,
                     &self.get_range_sum(*cur.start()..=m, range.clone(), p * 2),
                 );
             }
             if range.end > m + 1 {
-                sum = O::combine(
+                sum = self.monoid.combine(
                     &sum,
                     &self.get_range_sum(m + 1..=*cur.end(), range.clone(), p * 2 + 1),
                 );
@@ -113,7 +110,7 @@ impl<T: Clone, O: Operation<T>> SegTree<T, O> {
 
     fn update_impl(&mut self, cur: RangeInclusive<usize>, range: Range<usize>, p: usize, val: &T) {
         if cur.start() == cur.end() {
-            self.store[p - 1] = O::combine(&self.store[p - 1], val);
+            self.store[p - 1] = self.monoid.combine(&self.store[p - 1], val);
             return;
         }
         let m = cur.start() + (cur.end() - cur.start()) / 2;
@@ -123,7 +120,9 @@ impl<T: Clone, O: Operation<T>> SegTree<T, O> {
         if m + 1 < range.end {
             self.update_impl(m + 1..=*cur.end(), range, p * 2 + 1, val);
         }
-        self.store[p - 1] = O::combine(&self.store[p * 2 - 1], &self.store[p * 2]);
+        self.store[p - 1] = self
+            .monoid
+            .combine(&self.store[p * 2 - 1], &self.store[p * 2]);
     }
 }
 
@@ -133,28 +132,28 @@ mod test {
 
     #[test]
     fn build() {
-        let tree = SegTree::<_, Sum>::new(vec![10, 11, 12, 13, 14]);
+        let tree = SegTree::new(vec![10, 11, 12, 13, 14], Sum);
         assert_eq!(tree.store, vec![60, 33, 27, 21, 12, 13, 14, 10, 11]);
-        let tree = SegTree::<_, Sum>::new(vec![4, 3, 2, 1, 2, 3, 4]);
+        let tree = SegTree::new(vec![4, 3, 2, 1, 2, 3, 4], Sum);
         assert_eq!(tree.store, vec![19, 10, 9, 7, 3, 5, 4, 4, 3, 2, 1, 2, 3]);
-        let _ = SegTree::<i32, Sum>::new(vec![]);
+        let _ = SegTree::<i32, _>::new(vec![], Sum);
     }
 
     #[test]
     fn range_sum() {
-        let tree = SegTree::<_, Sum>::new(vec![10, 11, 12, 13, 14]);
+        let tree = SegTree::new(vec![10, 11, 12, 13, 14], Sum);
         assert_eq!(tree.query(3..5), Some(27));
         assert_eq!(tree.query(1..5), Some(50));
         assert_eq!(tree.query(..5), Some(60));
         assert_eq!(tree.query(..), Some(60));
         assert_eq!(tree.query(1..=3), Some(36));
-        let tree = SegTree::<i32, Sum>::new(vec![]);
+        let tree = SegTree::<i32, _>::new(vec![], Sum);
         assert_eq!(tree.query(..), None);
     }
 
     #[test]
     fn update() {
-        let mut tree = SegTree::<_, Sum>::new(vec![10, 11, 12, 13, 14]);
+        let mut tree = SegTree::new(vec![10, 11, 12, 13, 14], Sum);
         tree.update(.., 3);
         assert_eq!(tree.query(..), Some(13 + 14 + 15 + 16 + 17));
         tree.update(..3, 1);
--- src/operation.rs
+++ src/operation.rs
@@ -1,36 +1,36 @@
 use num_traits::{One, Zero};
 
-pub trait Operation<T> {
-    fn unit() -> T;
-    fn combine(lhs: &T, rhs: &T) -> T;
+pub trait Monoid<T> {
+    fn unit(&self) -> T;
+    fn combine(&self, lhs: &T, rhs: &T) -> T;
 }
 
 pub struct Sum;
 
-impl<N: Zero> Operation<N> for Sum
+impl<N: Zero> Monoid<N> for Sum
 where
     for<'a> &'a N: std::ops::Add<Output = N>,
 {
-    fn unit() -> N {
+    fn unit(&self) -> N {
         N::zero()
     }
 
-    fn combine(lhs: &N, rhs: &N) -> N {
+    fn combine(&self, lhs: &N, rhs: &N) -> N {
         lhs + rhs
     }
 }
 
 pub struct Product;
 
-impl<N: One> Operation<N> for Product
+impl<N: One> Monoid<N> for Product
 where
     for<'a> &'a N: std::ops::Mul<Output = N>,
 {
-    fn unit() -> N {
+    fn unit(&self) -> N {
         N::one()
     }
 
-    fn combine(lhs: &N, rhs: &N) -> N {
+    fn combine(&self, lhs: &N, rhs: &N) -> N {
         lhs * rhs
     }
 }
